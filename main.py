544970105e98b15cd6cf0ec45aa76662d288bcbb
Arif Budiono <37221706+arifbudiono@users.noreply.github.com>
2021-11-27 01:29:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import phonenumbers
+import opencage
+import folium
+from myphone import number
+
+from phonenumbers import geocoder
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+from phonenumbers import carrier
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+key = 'input your API key'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results =  geocoder.geocode(query)
+#print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lat,lng)
+
+myMap = folium.Map(location=[lat,lng], zoom_start= 9)
+folium.Marker([lat,lng], popup=location).add_to(myMap)
+
+myMap.save("mylocation.html")
